6f9cf08ca2280a255b90494cce331ece0f25042f
obinna <biostechnologyng@gmail.com>
2023-03-28 07:36:14 +0100

    Update

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const addUserHander = (event) => {
     event.preventDefault();
   };
   return (
-    <Card>
+    <Card className={classes.input}>
       <form onSubmit={addUserHander}>
         <label htmlFor="username">Username</label>
-        <imput id="username" type="text" />
+        <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
         <button type="submit">Add User</button>
